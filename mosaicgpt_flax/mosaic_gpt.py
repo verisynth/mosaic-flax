2c293ac817caa0ca32ad6cce2fc4157e984fe52f
Honglu Fan <honglu2875@gmail.com>
2023-03-26 02:19:26 +0000

    bug fix

--- mosaicgpt_flax/mosaic_gpt.py
+++ mosaicgpt_flax/mosaic_gpt.py
@@ -48,7 +48,8 @@ class FlaxAttention(nn.Module):
         assert ks >= qs, "key_len must be greater than or equal to query_len"
         return jnp.triu(jnp.ones((qs, ks), dtype=bool), ks - qs + 1)
 
-    def _attn_fn(self, query, key, value) -> tuple[jnp.ndarray, jnp.ndarray]:
+    def _attn_fn(self, query, key, value,
+                 past_position=0) -> tuple[jnp.ndarray, jnp.ndarray]:
         """
         Eager implementation of attention. c.f. `attention_ref(...)` in
         `https://github.com/HazyResearch/flash-attention/blob/main/tests/test_flash_attn.py`.
@@ -63,17 +64,25 @@ class FlaxAttention(nn.Module):
         key_len is normally the same as value_len, but could possibly larger than query_len (implementing kv cache).
         """
         attn_weights = jnp.einsum('...shd,...thd->...hst', query / jnp.sqrt(query.shape[-1]), key)
-
+        query_len = query.shape[-3]
+        key_len = key.shape[-3]
         attn_weights = jnp.where(self.causal,
                                  jnp.where(~self._get_causal_mask(query.shape[-3], key.shape[-3]),
                                            attn_weights,
                                            float('-inf')),
                                  attn_weights)
+        mask = jnp.arange(0, key_len, dtype=jnp.int32) < (key_len - past_position - query_len)
+        attn_weights = attn_weights - float('inf') * mask.reshape((1, 1, 1, -1))
         attn_weights = jax.nn.softmax(attn_weights, axis=-1)
         attn_output = jnp.einsum('...hst,...thd->...shd', attn_weights, value)
         return attn_output, attn_weights
 
-    def __call__(self, x, attn_mask=None, layer_past=None, use_cache=False, training=False) \
+    def __call__(self, x,
+                 attn_mask=None,
+                 layer_past=None,
+                 use_cache=False,
+                 past_position=0,
+                 training=False) \
             -> tuple[jnp.ndarray, jnp.ndarray, Optional[tuple]]:
         assert x.shape[-1] == self.d_model, \
             f"Input to Attention layer has different dimension than the hidden dimension. Got {x.shape[-1]}"
@@ -101,7 +110,7 @@ class FlaxAttention(nn.Module):
 
         present = (k, v)
 
-        context, attn_weights = self._attn_fn(q, k, v)
+        context, attn_weights = self._attn_fn(q, k, v, past_position=past_position)
 
         return self.out_proj(context.reshape(context.shape[:-2] + (self.d_model,))), attn_weights, present
 
@@ -147,11 +156,17 @@ class FlaxGPTBlock(nn.Module):
         self.resid_attn_dropout = nn.Dropout(self.resid_pdrop)
         self.resid_mlp_dropout = nn.Dropout(self.resid_pdrop)
 
-    def __call__(self, x, attn_mask=None, layer_past=None, use_cache=False, training=False) -> tuple:
+    def __call__(self, x,
+                 attn_mask=None,
+                 layer_past=None,
+                 use_cache=False,
+                 past_position=0,
+                 training=False) -> tuple:
         a = self.ln_1(x)
         b, _, present = self.causal_attn(a, attn_mask,
                                          layer_past=layer_past,
                                          use_cache=use_cache,
+                                         past_position=past_position,
                                          training=training)
 
         x = x + self.resid_attn_dropout(b, deterministic=not training)
@@ -197,9 +212,10 @@ class FlaxMosaicGPT(nn.Module):
 
     def __call__(self,
                  input_ids: jnp.ndarray,
+                 attention_mask: Optional[jnp.ndarray] = None,
                  past_key_values: Optional[jnp.ndarray] = None,
                  use_cache: bool = False,
-                 cache_pos: int = -1,
+                 past_position: int = 0,
                  training: bool = False) -> tuple[jnp.ndarray, Optional[list[jnp.ndarray]]]:
         if isinstance(input_ids, list):
             input_ids = jnp.array(input_ids)
@@ -210,13 +226,21 @@ class FlaxMosaicGPT(nn.Module):
         # past_key_values is supposed to be list(tuple(Array, Array))
         if past_key_values is None:
             past_key_values = [None] * self.n_layers
-            past_position = 0
+            assert past_position == 0, \
+                f"past_position must be 0 if past_key_values is None. Got {past_position}."
         else:
             assert len(past_key_values) == self.n_layers, \
                 f"length of past_key_values must match the number of layers. Got {len(past_key_values)}."
             # get the key tensor whose spec should be (batch, seq, n_head, head_dim), and
             # collect the `seq`, so that we shift the position embedding later.
-            past_position = jnp.where(cache_pos < 0, past_key_values[0][0].shape[1], cache_pos)
+            cache_len = past_key_values[0][0].shape[1]
+            for layer_past in past_key_values:
+                assert layer_past[0].shape == (batch_size, cache_len, self.n_heads, self.d_model // self.n_heads)
+                assert layer_past[1].shape == (batch_size, cache_len, self.n_heads, self.d_model // self.n_heads)
+
+        if attention_mask is None:
+            attention_mask = jnp.ones((batch_size, current_seq_len), dtype=jnp.int32)
+        assert attention_mask.shape == (batch_size, current_seq_len)
 
         tok_emb = self.wte(input_ids)
 
@@ -225,12 +249,13 @@ class FlaxMosaicGPT(nn.Module):
         x = tok_emb + pos_emb
         x = self.emb_drop(x, deterministic=not training)  # type: ignore
 
-        # TODO: Adding attention mask. Should be trivial but I'm lazy.
-        attn_mask = None
-
         present_key_values = []
         for block, layer_past in zip(self.blocks, past_key_values):  # type: ignore
-            x, present = block(x, attn_mask, layer_past=layer_past, use_cache=use_cache, training=training)
+            x, present = block(x, attention_mask,
+                               layer_past=layer_past,
+                               use_cache=use_cache,
+                               past_position=past_position,
+                               training=training)
             present_key_values.append(present)
 
         x = self.ln_f(x)  # type: ignore
